7b9ef62aa81ddd533a227013fc2fb4e6e552c1f6
The_Stallion <111058733+NotTheStallion@users.noreply.github.com>
2025-12-14 05:53:59 +0100

    clean : removing unused files

--- scripts/split_file.py
+++
@@ -1,6 +0,0 @@
-from safetensors_manager.file_utils import get_safetensor_files
-from safetensors_manager.splitter import split_safetensor_file
-
-if __name__=="__main__":
-    safetensor_file = "scripts/global/model.safetensors"
-    split_safetensor_file(safetensor_file, 7, verbose=True)
